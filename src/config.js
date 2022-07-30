bd464f8d20b4d1b6ea3e28ebd25a1c05f018c04d
Parker Johnson <carverj95@gmail.com>
2022-07-30 10:43:21 -0400

    remove node_modules from gitignore because i'm out of ideas

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let config = {}
 config.API = ()=>{
- //   return "https://localhost:7073"
-  return "http://tweetbook.parkerjohnson-projects.com"
+    return "https://localhost:7073"
+ // return "http://tweetbook.parkerjohnson-projects.com"
 }
 export default config;
